ae1b3abd5f2c9db399ebe5eea090b6ea2782304a
astraposh0001 <astraposh0001@gmail.com>
2024-12-29 14:19:19 +0530

    v1.7.5U

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 
 // CORS configuration
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
